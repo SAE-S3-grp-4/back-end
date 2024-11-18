32f97806a6a80d629c933ec131ce2099ed322940
Ewen <ewen.pichoff@gmail.com>
2024-11-18 10:34:44 +0100

    cobnstant

--- php/constantes.php
+++ php/constantes.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', 'inf2pj04');
-define('DB_PASSWORD', 'raiTur3iej');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'inf2pj_04');
-define('DB_SERVER', 'https://la-projets.univ-lemans.fr/pj-pma');
+define('DB_SERVER', 'localhost');
 ?>
