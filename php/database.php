d474829c86412e86e77e98e3fb471734fa5e5c6c
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-11-18 10:25:56 +0100

    aaa

--- php/database.php
+++ php/database.php
@@ -1,12 +1,7 @@
 <?php
 
-require_once('./constantes.php');
+require_once('constantes.php');
 
-//----------------------------------------------------------------------------
-//--- dbConnect --------------------------------------------------------------
-//----------------------------------------------------------------------------
-// Create the connection to the database.
-// \return False on error and the database otherwise.
 function dbConnect()
 {
   try {
@@ -23,114 +18,7 @@ function dbConnect()
   return $db;
 }
 
-//----------------------------------------------------------------------------
-//--- dbRequestPolls ---------------------------------------------------------
-//----------------------------------------------------------------------------
-// Function to get the polls.
-// \param db The connected database.
-// \return The list of polls titles.
-function dbRequestPhotos($db)
-{
-  try {
-    $request = 'SELECT small FROM photos';
-    $statement = $db->prepare($request);
-    $statement->execute();
-    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-  } catch (PDOException $exception) {
-    error_log('Request error: ' . $exception->getMessage());
-    return false;
-  }
-  return $result;
-}
 
-
-//----------------------------------------------------------------------------
-//--- dbRequestPoll ----------------------------------------------------------
-//----------------------------------------------------------------------------
-// Function to get a specific poll.
-// \param db The connected database.
-// \param id The id of the wanted poll.
-// \return The poll data.
-function dbRequestPhoto($db, $id)
-{
-  try {
-    $request = 'SELECT title, large FROM photos WHERE id=:id';
-    $statement = $db->prepare($request);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->execute();
-    $result = $statement->fetch(PDO::FETCH_ASSOC);
-  } catch (PDOException $exception) {
-    error_log('Request error: ' . $exception->getMessage());
-    return false;
-  }
-  return $result;
-}
-
-function dbRequestComments($db, $photoId)
-{
-  try {
-    $request = 'SELECT * FROM comments';
-    if ($photoId != '')
-      $request .= ' WHERE photoId=:photoId';
-    $statement = $db->prepare($request);
-    if ($photoId != '')
-      $statement->bindParam(':photoId', $photoId, PDO::PARAM_STR, 20);
-    $statement->execute();
-    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-  } catch (PDOException $exception) {
-    error_log('Request error: ' . $exception->getMessage());
-    return false;
-  }
-  return $result;
-}
-
-function dbAddComment($db, $login, $photoId, $text)
-{
-  try {
-    $request = 'INSERT INTO comments(userLogin, photoId, comment) VALUES(:userLogin, :photoId, :comment)';
-    $statement = $db->prepare($request);
-    $statement->bindParam(':userLogin', $login, PDO::PARAM_STR, 20);
-    $statement->bindParam(':photoId', $photoId, PDO::PARAM_INT, 20);
-    $statement->bindParam(':comment', $text, PDO::PARAM_STR, 80);
-    $statement->execute();
-  } catch (PDOException $exception) {
-    error_log('Request error: ' . $exception->getMessage());
-    return false;
-  }
-  return true;
-}
-
-function dbDeleteComment($db, $id, $login)
-{
-  error_log($id . " " . $login);
-  try {
-    $request = 'DELETE FROM comments WHERE id=:id AND userLogin=:userLogin';
-    $statement = $db->prepare($request);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->bindParam(':userLogin', $login, PDO::PARAM_STR, 20);
-    $statement->execute();
-  } catch (PDOException $exception) {
-    error_log('Request error: ' . $exception->getMessage());
-    return false;
-  }
-  return true;
-}
-
-function dbModifyComment($db, $id, $login, $text)
-{
-  try {
-    $request = 'UPDATE comments SET comment=:comment WHERE id=:id AND userLogin=:userLogin';
-    $statement = $db->prepare($request);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->bindParam(':userLogin', $login, PDO::PARAM_STR, 20);
-    $statement->bindParam(':comment', $text, PDO::PARAM_STR, 80);
-    $statement->execute();
-  } catch (PDOException $exception) {
-    error_log('Request error: ' . $exception->getMessage());
-    return false;
-  }
-  return true;
-}
 
 function dbRequestUser($db)
 {
--- php/request.php
+++ php/request.php
@@ -23,44 +23,7 @@ if ($id == '')
   $id = NULL;
 $data = false;
 
-// Request Photos.
-if ($requestRessource == 'photos') {
-  if ($id != NULL)
-    $data = dbRequestPhoto($db, intval($id));
-  else
-    $data = dbRequestPhotos($db);
-}
 
-
-if ($requestMethod == "GET") {
-  if ($requestRessource == "comments") {
-    if (isset($_GET["photoId"])) {
-      $data = dbRequestComments($db, $_GET["photoId"]);
-    }
-  }
-}
-
-if ($requestMethod == "POST") {
-  if ($requestRessource == "comments") {
-    $textToPost = filter_var($_POST["comment"], FILTER_SANITIZE_STRING);
-    $data = dbAddComment($db, $_POST["userLogin"], $_POST["photoId"], $textToPost);
-  }
-}
-
-if ($requestMethod == "PUT") {
-  if ($requestRessource == "comments") {
-    $put_data = file_get_contents("php://input");
-    parse_str($put_data, $post_vars);
-    $textToPost = filter_var($post_vars["comment"], FILTER_SANITIZE_STRING);
-    $data = dbModifyComment($db, $id, $login, $textToPost);
-  }
-}
-
-if ($requestMethod == "DELETE") {
-  if ($requestRessource == "comments") {
-    $data = dbDeleteComment($db, $id, $_GET["userLogin"]);
-  }
-}
 
 // Send data to the client.
 header('Content-Type: application/json; charset=utf-8');
