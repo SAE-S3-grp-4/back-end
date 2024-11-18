d7609e589d094bbc7090dc8191fd8ab14b9bf961
Ewen <ewen.pichoff@gmail.com>
2024-11-18 15:11:30 +0100

    c'est pas le quartier qui me quiiiiitee c'est moi jquitte le quartierrrrrrrrrrrr

--- php/request.php
+++ php/request.php
@@ -4,9 +4,11 @@ require_once('database.php');
 
 // Database connexion.
 $db = dbConnect();
+
 $login = 'cir2';
 
-if (!$db) {
+if (!$db)
+{
   header('HTTP/1.1 503 Service Unavailable');
   exit;
 }
@@ -23,15 +25,55 @@ if ($id == '')
   $id = NULL;
 $data = false;
 
+// Request Photos.
+if ($requestRessource == 'photos')
+{
+  if ($id != NULL)
+    $data = dbRequestPhoto($db, intval($id));
+  else
+    $data = dbRequestPhotos($db);
+}
 
+
+if($requestMethod == "GET"){
+  if($requestRessource == "comments"){
+      if(isset($_GET["photoId"])){
+          $data = dbRequestComments($db,$_GET["photoId"]);
+      }
+  }
+}
+
+if($requestMethod == "POST"){
+  if($requestRessource == "comments"){
+      $textToPost = filter_var($_POST["comment"], FILTER_SANITIZE_STRING);
+      $data = dbAddComment($db, $_POST["userLogin"], $_POST["photoId"],$textToPost);
+  }
+}
+
+if($requestMethod == "PUT"){
+  if($requestRessource == "comments"){
+      $put_data = file_get_contents("php://input");
+      parse_str($put_data, $post_vars);
+      $textToPost = filter_var($post_vars["comment"], FILTER_SANITIZE_STRING);
+      $data = dbModifyComment($db, $id, $login, $textToPost);
+  }
+}
+
+if($requestMethod == "DELETE"){
+  if($requestRessource == "comments"){
+      $data = dbDeleteComment($db,$id,$_GET["userLogin"]);
+  }
+}
 
 // Send data to the client.
 header('Content-Type: application/json; charset=utf-8');
 header('Cache-control: no-store, no-cache, must-revalidate');
 header('Pragma: no-cache');
-if ($data !== false) {
+if ($data !== false)
+{
   header('HTTP/1.1 200 OK');
   echo json_encode($data);
-} else
+}
+else
   header('HTTP/1.1 400 Bad Request');
 exit;
