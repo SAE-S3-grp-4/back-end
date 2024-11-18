bfff1b156e54ae771ab9e933cebb64c1d60bbde1
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-11-18 09:40:52 +0100

    Update database connection

--- constantes.php
+++ constantes.php
@@ -1,6 +1,6 @@
 <?php
-    define('DB_USER', 'comweb_project');
-    define('DB_PASSWORD', 'tcejorp_bewmoc_isen29');
-    define('DB_NAME', 'comweb_project');
-    define('DB_SERVER', 'localhost');
+define('DB_USER', 'inf2pj04');
+define('DB_PASSWORD', 'raiTur3iej');
+define('DB_NAME', 'inf2pj_04');
+define('DB_SERVER', 'https://la-projets.univ-lemans.fr/pj-pma');
 ?>
