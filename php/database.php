bb86ff5f212d84d565afde485af7289deb93497a
Guillaume Dodin <gdodin53@gmail.com>
2024-11-21 16:12:22 +0100

    gestion boutique complet

--- php/database.php
+++ php/database.php
@@ -64,21 +64,20 @@ function dbRequestProduct($db)
 
 function dbAddProduct($db, $nom, $desc, $img, $prix, $stock)
 {
-  try {
-    $request = 'INSERT INTO PRODUIT(Nom_Produit, Description_Produit, Img_Produit, Prix_Produit, Stock_Produit) VALUES(:Nom_Produit, :Description_Produit, :Img_Produit, :Prix_Produit, :Stock_Produit)';
-    $statement = $db->prepare($request);
-    $statement->bindParam(':Nom_Produit', $nom, PDO::PARAM_STR, 50);
-    $statement->bindParam(':Description_Produit', $desc, PDO::PARAM_STR, 200);
-    $statement->bindParam(':Img_Produit', $img, PDO::PARAM_STR, 200);
-    $statement->bindParam(':Prix_Produit', $prix, PDO::PARAM_INT, 10);
-    $statement->bindParam(':Stock_Produit', $stock, PDO::PARAM_INT, 10);
-    //var_dump($statement);
-    $statement->execute();
-  } catch (PDOException $exception) {
-    error_log('Request error:' . $exception->getMessage());
-    return false;
-  }
-  return true;
+    try {
+        $request = 'INSERT INTO PRODUIT(Nom_Produit, Description_Produit, Img_Produit, Prix_Produit, Stock_Produit) VALUES(:Nom_Produit, :Description_Produit, :Img_Produit, :Prix_Produit, :Stock_Produit)';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':Nom_Produit', $nom, PDO::PARAM_STR, 50);
+        $statement->bindParam(':Description_Produit', $desc, PDO::PARAM_STR, 200);
+        $statement->bindParam(':Img_Produit', $img, PDO::PARAM_STR, 200);
+        $statement->bindParam(':Prix_Produit', $prix, PDO::PARAM_INT, 10);
+        $statement->bindParam(':Stock_Produit', $stock, PDO::PARAM_INT, 10);
+        $statement->execute();
+    } catch (PDOException $exception) {
+        error_log('Request error:' . $exception->getMessage());
+        return false;
+    }
+    return true;
 }
 
 function dbAddEvent($db, $nom, $desc, $date, $prix, $nbPlace, $dateFinInscription)
@@ -103,14 +102,29 @@ function dbAddEvent($db, $nom, $desc, $date, $prix, $nbPlace, $dateFinInscriptio
 
 function dbDeleteProduct($db, $id)
 {
-  try {
-    $request = 'DELETE FROM produit WHERE Id_Produit = :id';
-    $statement = $db->prepare($request);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->execute();
-  } catch (PDOException $exception) {
-    error_log('Request error :' . $exception->getMessage());
-  }
+    try {
+        // Update the status of related orders to "Produit supprimé"
+        $updateOrderStatus = 'UPDATE commande SET Statut_Commande = "Produit supprimé" WHERE Id_Commande IN (SELECT Id_Commande FROM bon_de_commande WHERE Id_Produit = :id)';
+        $statement = $db->prepare($updateOrderStatus);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        // Delete related order slips
+        $deleteOrderSlips = 'DELETE FROM bon_de_commande WHERE Id_Produit = :id';
+        $statement = $db->prepare($deleteOrderSlips);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        // Delete the product itself
+        $deleteProduct = 'DELETE FROM produit WHERE Id_Produit = :id';
+        $statement = $db->prepare($deleteProduct);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+    } catch (PDOException $exception) {
+        error_log('Request error: ' . $exception->getMessage());
+        return false;
+    }
+    return true;
 }
 
 function dbDeleteEvent($db, $id)
@@ -138,5 +152,45 @@ function dbDeleteEvent($db, $id)
         return false;
     }
     return true;
+}
+
+function dbModifyProduct($db, $id, $nom, $desc, $img, $prix, $stock)
+{
+    try {
+        if ($img) {
+            $request = 'UPDATE PRODUIT SET Nom_Produit = :Nom_Produit, Description_Produit = :Description_Produit, Img_Produit = :Img_Produit, Prix_Produit = :Prix_Produit, Stock_Produit = :Stock_Produit WHERE Id_Produit = :Id_Produit';
+            echo $request;
+            $statement = $db->prepare($request);
+            $statement->bindParam(':Img_Produit', $img, PDO::PARAM_STR, 250);
+        } else {
+            $request = 'UPDATE PRODUIT SET Nom_Produit = :Nom_Produit, Description_Produit = :Description_Produit, Prix_Produit = :Prix_Produit, Stock_Produit = :Stock_Produit WHERE Id_Produit = :Id_Produit';
+            $statement = $db->prepare($request);
+        }
+        $statement->bindParam(':Id_Produit', $id, PDO::PARAM_INT);
+        $statement->bindParam(':Nom_Produit', $nom, PDO::PARAM_STR, 50);
+        $statement->bindParam(':Description_Produit', $desc, PDO::PARAM_STR, 150);
+        $statement->bindParam(':Prix_Produit', $prix, PDO::PARAM_STR, 15);
+        $statement->bindParam(':Stock_Produit', $stock, PDO::PARAM_INT);
+        $statement->execute();
+    } catch (PDOException $exception) {
+        error_log('Request error: ' . $exception->getMessage());
+        return false;
+    }
+    return true;
+}
+
+function dbRequestProductById($db, $id)
+{
+    try {
+        $request = 'SELECT * FROM PRODUIT WHERE Id_Produit = :Id_Produit';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':Id_Produit', $id, PDO::PARAM_INT);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $exception) {
+        error_log('Request error: ' . $exception->getMessage());
+        return false;
+    }
+    return $result;
 }
 
