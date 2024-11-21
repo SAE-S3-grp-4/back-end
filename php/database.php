267668964c04553b5120f40098093170a9f4a9ad
Ewen <ewen.pichoff@gmail.com>
2024-11-21 10:15:25 +0100

    DELETE PRODUCT

--- php/database.php
+++ php/database.php
@@ -102,7 +102,7 @@ function dbAddEvent($db, $nom, $desc, $date, $prix)
 function dbDeleteProduct($db, $id)
 {
   try {
-    $request = 'DELETE FROM produit WHERE id = :id';
+    $request = 'DELETE FROM produit WHERE Id_Produit = :id';
     $statement = $db->prepare($request);
     $statement->bindParam(':id', $id, PDO::PARAM_INT);
     $statement->execute();
